586b570b32ec09392afda2624472d01e329aa4fd
ojitos369 <ojitos369@gmail.com>
2023-08-24 18:02:15 -0600

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use gtk4 as gtk;
+use gtk::prelude::*;
+use gtk::{glib, Application, ApplicationWindow, Button};
+
+fn main() -> glib::ExitCode {
+    let application = Application::builder()
+        .application_id("com.example.FirstGtkApp")
+        .build();
+
+    application.connect_activate(|app| {
+        let window = ApplicationWindow::builder()
+            .application(app)
+            .title("First GTK Program")
+            .default_width(350)
+            .default_height(70)
+            .build();
+
+        let button = Button::with_label("Click me!");
+        button.connect_clicked(|_| {
+            // notify-send "Hello World!" "The button was clicked!"
+            std::process::Command::new("notify-send")
+                .arg("Hello World!")
+                .arg("The button was clicked!")
+                .spawn()
+                .expect("Failed to execute process");
+
+        });
+        window.set_child(Some(&button));
+
+        window.present();
+    });
+
+    application.run()
 }
